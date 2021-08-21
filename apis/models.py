2d2569278df62e6b4e39dab8a57e07d43f48536c
joelmanibi <bi.mani@uvci.edu.ci>
2021-08-21 22:45:50 +0000

    first commit

---
+++ apis/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
+# Create your models here.
+
+#class AgentProfile(models.Model):
+    
+    #user = models.OneToOneField(User, null=True, on_delete= models.CASCADE)
+    #Tel = PhoneNumberField(blank=True)
+    #def __str__(self):
+        #return self.user.first_name
+
