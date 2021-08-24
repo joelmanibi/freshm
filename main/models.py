14cef6134dfb714302ee384391efa9bbe46263ba
joelmanibi <bi.mani@uvci.edu.ci>
2021-08-24 11:00:03 +0000

    static

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 class Client(models.Model):
     
