46dd4541738815b6d5831908e4bd0e8353d2639d
joelmanibi <bi.mani@uvci.edu.ci>
2021-08-24 12:30:39 +0000

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Produit,client,Commande
+
+admin.site.register(Client)
+admin.site.register(Commande)
+admin.site.register(Produit)
