630ae48366c2da386058a6956bab92eeb7e0425b
joelmanibi <bi.mani@uvci.edu.ci>
2021-08-24 12:38:39 +0000

    Update urls.py

--- apis/urls.py
+++ apis/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import UserRecordView,ListProduit, DetailProduit
 
-app_name = 'apis'
+
 urlpatterns = [
     path('user/', UserRecordView.as_view(), name='users'),
     path('', ListProduit.as_view()),
