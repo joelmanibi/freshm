a84dac46dd8c49654dad9d32991e7fd654b4c75a
joelmanibi <bi.mani@uvci.edu.ci>
2021-08-24 16:41:01 +0000

    Update urls.py

--- freshm/urls.py
+++ freshm/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from rest_framework.authtoken import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apis.urls')),
@@ -13,3 +14,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
