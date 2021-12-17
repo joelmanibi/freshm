ef5b33377ed2ee208a79526147ff53f9a80f8027
joelmanibi <bi.mani@uvci.edu.ci>
2021-12-17 13:44:33 +0000

    Update views.py

--- apis/views.py
+++ apis/views.py
@@ -78,8 +78,8 @@ class ListCommandeAgent(generics.ListCreateAPIView):
         pour l'utilisateur actuellement authentifié.
         """
         user = self.request.user
-        produit = models.Produit.objects.filter(agent=user)
-        return models.Commande.objects.filter(produit=produit)
+        prod = models.Produit.objects.filter(agent=user)
+        return models.Commande.objects.filter(produit=prod)
 
 class DetailProduit(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Produit.objects.all()
