6e0f08c0c665b7396b5786929736046535273439
joelmanibi <bi.mani@uvci.edu.ci>
2022-04-05 15:40:13 +0000

    Update views.py

--- apis/views.py
+++ apis/views.py
@@ -69,7 +69,7 @@ class ListProduitAgent(generics.ListCreateAPIView):
         pour l'utilisateur actuellement authentifié.
         """
         user = self.request.user
-        return models.Produit.objects.filter(agent=user)
+        return models.Produit.objects.filter(agent=user)[0]
 
 class ListCommandeAgent(generics.ListCreateAPIView):
     serializer_class = CommandeSerializer
