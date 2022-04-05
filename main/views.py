4f004dd06dff3466b2e2d04d8c1503d52527b7cc
joelmanibi <bi.mani@uvci.edu.ci>
2022-04-05 17:35:09 +0000

    Update views.py

--- main/views.py
+++ main/views.py
@@ -41,7 +41,7 @@ def signin_client(request):
             login(request,user)
             if 'next' in request.POST:
                 return redirect(request.POST.get('next'))
-            
+            return redirect("index")
     else:
         form = AuthenticationForm()
     return render(request, 'main/signinclient.html',{'form':form})
